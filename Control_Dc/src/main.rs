20e87f781fea2f9bc337d52c1e6af17fad82a98a
Loken1016 <alexre0812@gmail.com>
2025-10-10 23:54:00 -0500

    Tercer commit

---
+++ Control_Dc/src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use arduino_hal::simple_pwm::*;
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    let timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
+
+    let mut led3 = pins.d9.into_output().into_pwm(&timer1);
+    led3.enable();
+    led3.set_duty(led3.get_max_duty() );
+
+    let _led1 = pins.d7.into_output().set_high();
+    let _led2 = pins.d8.into_output().set_low() ;
+
+
+    loop { }
+}
+
+
