b8fee62632fbc954073f7a3d696e34098bd9053d
Loken <alexre0812@gmail.com>
2025-12-03 00:29:49 -0500

    Guardar cambios locales

--- correct/pwm2/src/main.rs
+++ correct/pwm2/src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::simple_pwm::*;
+use embedded_hal::delay::DelayNs;
+use embedded_hal::pwm::SetDutyCycle;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -8,20 +11,35 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
+    let timer2 = Timer2Pwm::new(dp.TC2, Prescaler::Prescale64);
 
-    let mut led = pins.d13.into_output();
+    let mut led1 = pins.d9.into_output().into_pwm(&timer1);
+    let mut led2 = pins.d10.into_output().into_pwm(&timer1);
+    let mut led3 = pins.d11.into_output().into_pwm(&timer2);
+
+    let mut delay = arduino_hal::Delay::new();
+
+    led1.enable();
+    led2.enable();
+    led3.enable();
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        fade(&mut led1, &mut delay);
+        fade(&mut led2, &mut delay);
+        fade(&mut led3, &mut delay);
     }
+
 }
+
+fn fade(led: &mut impl SetDutyCycle, delay: &mut arduino_hal::Delay)-> () {
+     for pct in 0..=50 {
+        led.set_duty_cycle_percent(pct).ok();
+        delay.delay_ms(60);
+     }   
+     for pct in (0..=50).rev() {
+        led.set_duty_cycle_percent(pct).ok();
+        delay.delay_ms(60);
+     }
+     led.set_duty_cycle_percent(0).ok();
+    }
