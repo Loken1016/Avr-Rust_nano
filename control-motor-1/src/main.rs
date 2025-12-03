21e7f865c4e572acd2ea91aae7219a7d7c2cc897
Loken <alexre0812@gmail.com>
2025-12-03 16:05:12 -0500

    Guardar cambios locales

--- control-motor-1/src/main.rs
+++ control-motor-1/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::simple_pwm::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -8,20 +9,15 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
 
-    let mut led = pins.d13.into_output();
+    let mut led3 = pins.d9.into_output().into_pwm(&timer1);
+    led3.enable();
+    led3.set_duty(led3.get_max_duty() );
 
-    loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
-    }
+    let _led1 = pins.d7.into_output().set_high();
+    let _led2 = pins.d8.into_output().set_low() ;
+
+
+    loop { }
 }
